a97d7fb97ba8e57facddb1d17cb3b6c78cbe4e87
Kurt <kurtanthonylane@hotmail.com>
2023-11-07 13:42:39 +1000

    DAY 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+result.uppercased()
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let score = 10
+let reallyBig = 1_00_00_00
+
+var counter = 10
+counter += 5
+print(counter)
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print (number.isMultiple(of: 3))
+print (120.isMultiple(of: 3))
+
+let double = 0.1 + 0.2
+print(number)
